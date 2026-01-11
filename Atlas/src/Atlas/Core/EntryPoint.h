b999fe21ce6bd7fc5ab23e9944ad8729221990ca
Jared Lipkin <jaredlipkin@gmail.com>
2026-01-11 13:43:26 -0500

    Remove TODO tracking

--- Atlas/src/Atlas/Core/EntryPoint.h
+++ Atlas/src/Atlas/Core/EntryPoint.h
@@ -11,15 +11,15 @@ int main(int argc, char** argv) {
 
     AT_CORE_TRACE("Logging initialized");
 
-    AT_PROFILE_BEGIN_SESSION("Startup", "AtlasProfile-Startup.json");
+    AT_PROFILE_BEGIN_SESSION("Startup", "AtlasProfile.Startup.json");
     auto app = Atlas::createApplication();
     AT_PROFILE_END_SESSION();
 
-    AT_PROFILE_BEGIN_SESSION("Runtime", "AtlasProfile-RunTime.json");
+    AT_PROFILE_BEGIN_SESSION("Runtime", "AtlasProfile.RunTime.json");
     app->run();
     AT_PROFILE_END_SESSION();
 
-    AT_PROFILE_BEGIN_SESSION("Shutdown", "AtlasProfile-Shutdown.json");
+    AT_PROFILE_BEGIN_SESSION("Shutdown", "AtlasProfile.Shutdown.json");
     delete app;
     AT_PROFILE_END_SESSION();
 }
--- playground.cpp
+++ playground.cpp
@@ -1,66 +1,16 @@
+#include <metal-cpp/Metal.hpp>
+// per pipeline
+MTL::ArgumentEncoder* argEnc = fs->newArgumentEncoder(1);
+MTL::Buffer* uBuf = MetalContext::getMTLDevice()->newBuffer(argEnc->encodedLength(), MTL::ResourceStorageModeShared);
+argEnc->setArgumentBuffer(uBuf, 0);
+NS::UInteger length = argEnc->encodedLength();
 
-void MetalContext::beginFrame() {
-    m_pool = NS::AutoreleasePool::alloc()->init();
-    s_drawable = m_layer->nextDrawable();
-    if (!s_drawable) {
-        AT_WARN("CA::MetalDrawable* not drawable (MetalContext)");
-        return;
-    }
-}
 
-void MacOSWindow::onUpdate() {
-    glfwPollEvents();
-    m_data.context->swapBuffers();
-}
+// color data
+glm::vec4 c{0.8f, 0.1f, 0.1f, 1.0f};
 
-void MetalRendererAPI::setClearColor(const glm::vec4& color) {
-    MTL::ClearColor clr = MTL::ClearColor::Make((double)color.r, (double)color.g, (double)color.b, (double)color.a);
-    m_passDesc->colorAttachments()->object(0)->setClearColor(clr);
-}
+// per update
+memcpy(argEnc->constantData(0), &c, sizeof(c));        // constantData( n ) == shader [[id( n )]] ---- c == data to upload
 
-void MetalRendererAPI::clear() {
-    // Ensure color attachment uses the drawable's texture
-    MTL::RenderPassColorAttachmentDescriptor* attachment =
-        m_passDesc->colorAttachments()->object(0);
-    attachment->setTexture(m_drawable->texture());
-    attachment->setLoadAction(MTL::LoadActionClear);
-    attachment->setStoreAction(MTL::StoreActionStore);
-
-    // Encode
-    m_commandBuffer = MetalContext::getCommandQueue()->commandBuffer();
-    s_commandEncoder = m_commandBuffer->renderCommandEncoder(m_passDesc);
-}
-
-// Renderer::beginScene();
-
-void MetalShader::bind() const {
-    MTL::RenderCommandEncoder* encoder = MetalRendererAPI::getMTLEncoder();
-    encoder->setRenderPipelineState(m_pipelineState);
-}
-
-void MetalVertexArray::bind() const {
-    
-    for (auto buffer : m_vertexBuffers) {
-        MetalRendererAPI::getMTLEncoder()->setVertexBuffer(buffer, 0, index);
-    }
-}
-
-void MetalRendererAPI::drawIndexed(const std::shared_ptr<VertexArray>& vertexArray) {
-    std::shared_ptr<MetalIndexBuffer> MIB = std::static_pointer_cast<MetalIndexBuffer>(vertexArray->getIndexBuffer());
-    MTL::Buffer* indexBuffer = MIB->getBuffer();
-    s_commandEncoder->drawIndexedPrimitives(MTL::PrimitiveTypeTriangle, vertexArray->getIndexBuffer()->getCount(), MTL::IndexTypeUInt32, indexBuffer, 0);
-}
-
-// Renderer::endScene();
-
-void MetalRendererAPI::commit() {
-    // Present the drawable
-    s_commandEncoder->endEncoding();
-
-    m_commandBuffer->presentDrawable((MTL::Drawable*)MetalContext::getMTLDrawable());
-    m_commandBuffer->commit();
-}
-
-void MetalContext::endFrame() {
-    m_pool->release();
-}
+// per draw
+m_encoder->setFragmentBuffer(p.getFragBuf(), 0, 1);
