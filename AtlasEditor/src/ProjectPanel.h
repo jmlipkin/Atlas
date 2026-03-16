472aaeeb58c50336399b49434fad366caaecd5c6
Jared Lipkin <jaredlipkin@gmail.com>
2026-03-15 21:45:43 -0400

    Visible selections in ProjectPanel

--- AtlasEditor/src/ProjectPanel.h
+++ AtlasEditor/src/ProjectPanel.h
@@ -20,19 +20,27 @@ class ProjectPanel {
 		ImGui::Begin("Project");
 		ImGuiSystem::DrawPanelAccentBar(ImGuiSystem::PanelAccent::Purple);
 
+		ImGuiIO io = ImGui::GetIO();
+		ImGui::PushFont(io.Fonts->Fonts[1]);
 		if (m_activeProject == nullptr) {
 			ImGui::Text("No active project");
+			ImGui::PopFont();
 			ImGui::End();
 			return;
 		}
-
+		
 		std::string projName = m_activeProject->getName();
 		ImGui::Text("%s", projName.c_str());
+		ImGui::PopFont();
 
 		for (auto s : m_activeProject->getData().scene_filepaths) {
 			std::string displayName = std::filesystem::path(s).stem().string();
-			if (ImGui::Selectable(displayName.c_str())) {
+
+			bool isSelected = m_selectionContext && ProjectManager::toAbsolutePath(s) == m_selectionContext->getPath();
+
+			if (ImGui::Selectable(displayName.c_str(), isSelected)) {
 				auto scene = ProjectManager::loadScene(ProjectManager::toAbsolutePath(s));
+				m_selectionContext = scene;
 				m_onSceneSelected(scene);
 			}
 		}
@@ -42,6 +50,7 @@ class ProjectPanel {
 
   private:
 	std::shared_ptr<Project> m_activeProject;
+	std::shared_ptr<Scene>	 m_selectionContext;
 	SceneSelectedCallback	 m_onSceneSelected;
 };
 
